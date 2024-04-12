44cf2bc813a2202d9fff79fddd3290bd15da9689
luyeeya <luyi95@foxmail.com>
2024-04-12 15:43:15 +0800

    update spring ioc config

--- spring-demo/src/main/java/spring/ioc/config/annotation_config/App.java
+++ spring-demo/src/main/java/spring/ioc/config/annotation_config/App.java
@@ -1,8 +1,5 @@
 package spring.ioc.config.annotation_config;
 
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.support.AbstractBeanDefinition;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -13,13 +10,5 @@ public class App {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AnnotationConfig.class);
         UserServiceImpl userService = (UserServiceImpl) applicationContext.getBean("userService");
         System.out.println(userService.listUser());
-
-        // 编程式注册 BeanDefinition
-        AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition().getBeanDefinition();
-        beanDefinition.setBeanClass(OrderService.class);
-        beanDefinition.setScope(BeanDefinition.SCOPE_SINGLETON);
-        applicationContext.registerBeanDefinition("orderService", beanDefinition);
-        OrderService orderService = (OrderService) applicationContext.getBean("orderService");
-        orderService.createOrder();
     }
 }
--- spring-demo/src/main/java/spring/ioc/config/annotation_config/OrderService.java
+++
@@ -1,7 +0,0 @@
-package spring.ioc.config.annotation_config;
-
-public class OrderService {
-    public void createOrder() {
-        System.out.println("Create order...");
-    }
-}
