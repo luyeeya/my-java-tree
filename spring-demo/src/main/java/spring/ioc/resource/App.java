d0b33ed8f1ba1847bea6820030f96d5fab3cb440
luyeeya <luyi95@foxmail.com>
2024-04-12 16:06:58 +0800

    Spring ApplicationContext 各种资源获取能力

---
+++ spring-demo/src/main/java/spring/ioc/resource/App.java
@@ -0,0 +1,33 @@
+package spring.ioc.resource;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+/**
+ * 展示 ApplicationContext 获取多种资源的能力（ResourcePatternResolver -> ResourceLoader）
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // URI
+        Resource uriResource = context.getResource("https://www.baidu.com");
+        System.out.println("resource: " + uriResource.getURL() + ", contentLength: " + uriResource.contentLength());
+
+        // File
+        Resource fileResource = context.getResource("file://D:\\projects\\my-java-tree\\spring-demo\\src\\main\\java\\spring\\ioc\\resource\\App.java");
+        System.out.println("resource: " + fileResource.getFilename() + ", contentLength: " + fileResource.contentLength());
+
+        // ClassPath
+        Resource classpathResource = context.getResource("classpath:application.properties");
+        System.out.println("resource: " + classpathResource.getFilename() + ", contentLength: " + classpathResource.contentLength());
+
+        // 多个资源
+        Resource[] resources = context.getResources("classpath:messages_*");
+        for (Resource resource : resources) {
+            System.out.println("resource: " + resource.getFilename() + ", contentLength: " + resource.contentLength());
+        }
+    }
+}
---
+++ spring-demo/src/main/java/spring/ioc/resource/AppConfig.java
@@ -0,0 +1,7 @@
+package spring.ioc.resource;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("spring.ioc.resource")
+public class AppConfig {
+}
